5f92c5f18ca97a034e4f2befedfb4168d73b5216
M-Sayer <haji.san@icloud.com>
2020-09-01 21:38:42 -0400

    refactored dashboard state into language context

--- src/routes/DashboardRoute/DashboardRoute.js
+++ src/routes/DashboardRoute/DashboardRoute.js
@@ -2,25 +2,24 @@ import React, { Component } from 'react'
 import LanguageApiService from '../../services/language-api-service';
 import './DashboardRoute.css'
 
+import LanguageContext from '../../contexts/LanguageContext';
 import WordTile from '../../components/Dashboard/WordTile';
 
 class DashboardRoute extends Component {
-  state = {
-    language: '',
-    words: '',
-  }
+  static contextType = LanguageContext;
 
   async componentDidMount() {
     const data = await LanguageApiService.getUserLanguage();
     console.log(data)
-    this.setState({
-      language: data.language,
+    this.context.setLanguage({
+      language: {...data.language},
       words: data.words,
     })
+    console.log(this.context)
   }
   
   renderWordTiles = () => {
-    if (this.state.words) return this.state.words.map((word, idx) => {
+    if (this.context.language.words) return this.context.language.words.map((word, idx) => {
       return (
         <WordTile key={idx} word={word} />
       )
@@ -31,7 +30,7 @@ class DashboardRoute extends Component {
     let correct = 0
     let incorrect = 0
     
-    if (this.state.words) this.state.words.forEach(word => {
+    if (this.context.language.words) this.context.language.words.forEach(word => {
       correct += word.correct_count
       incorrect += word.incorrect_count
     })
@@ -44,7 +43,7 @@ class DashboardRoute extends Component {
         implement and style me
         <div>
           language:
-          {this.state.language.name}
+          {this.context.language && this.context.language.language.name}
         </div>
         words for language:
         {this.renderWordTiles()}

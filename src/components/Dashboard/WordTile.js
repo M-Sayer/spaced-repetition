5af2f5897b2a4cb5471324322f618c864edfdee0
M-Sayer <haji.san@icloud.com>
2020-09-04 18:32:26 -0400

    implemented features according to tests

--- src/components/Dashboard/WordTile.js
+++ src/components/Dashboard/WordTile.js
@@ -3,32 +3,27 @@ import React from 'react';
 import { StyledWordTile } from './WordTile.styled';
 
 const WordTile = (props) => {
-  let word;
-  //word prop from dashboard route, displayWord prop from learn route
-  props.word ? word = props.word : word = props.displayWord;
 
   return (
     <section className='word-card'>
       <div className='wordInLang'>
         <h2>
-          {word.original || word.name}
-        </h2>
+          Translate the word:
+        </h2>   
+        <span>
+          {props.displayWord.name}
+        </span>
       </div>
-      {word.translation && <div className='wordInEng'>
-        <h4>
-          {word.translation}
-        </h4>
-      </div>}
       <div className='count'>
-        <span className='correctCount'>
-          {word.correct_count}
-        </span>
-        <span className='incorrectCount'>
-          {word.incorrect_count}
-        </span>
+        { props.displayWord &&
+          <p>You have answered this word correctly {props.displayWord} times</p>
+        }
+        { props.displayWord &&
+          <p>You have answered this word incorrectly {props.displayWord.incorrect_count} times</p>
+        }
         {props.totalScore >=0 && 
           <div className='totalScore'>
-            total score: {props.totalScore}
+            <p>Your total score is: {props.totalScore}</p>
           </div>}
       </div>
     </section>
